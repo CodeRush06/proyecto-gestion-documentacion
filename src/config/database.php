8bea2700d1216950e9654d559284ee568d807b2c
CodeRush06 <coderush06@gmail.com>
2026-08-23 18:57:12 -0300

    feat: Conexión de PHP a la Base de datos
    Conectamos php con la base de datos mediante un PDO.

--- src/config/database.php
+++ src/config/database.php
@@ -1,3 +1,23 @@
 <?php
-# Conexion a base de datos
+$host = "localhost";
+$db = "modulo1";
+$user = "root";
+$pass = "";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    // CORREGIDO: PDO::ERRMODE_EXCEPTION
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+
+    echo "Conexión exitosa a la base sigsm.<br>";
+
+} catch (PDOException $e) {
+    // Es mejor mostrar el error real para saber por qué falla (ej. contraseña incorrecta, base de datos no existe)
+    echo "Conexión no exitosa. Motivo: " . $e->getMessage();
+}
 ?>
